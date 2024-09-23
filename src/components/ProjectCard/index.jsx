41b197a6703d9af123748b5a444dc7f4cfd0fba2
gonzaloLT <gonzalobarroso50@gmail.com>
2024-09-23 04:49:12 -0300

    Se añadio opcion de hacer click en un proyecto

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const ProjectCard = ({project}) => {
   return (
-    <div>
-      <h2>{project.name}</h2>
-    </div>
+    <Link to={`/my-projects/${project.id}`} style={{ textDecoration: 'none' }}>
+        <div className='project-card'>
+            <h2>{project.name}</h2>
+        </div>
+    </Link>
   )
 }
