5d16ea94031b12cf53bf0848bcb6d876cc3933ff
Gonzalo Barroso <gonzalobarroso50@gmail.com>
2024-10-07 19:26:37 -0300

    Agregando funcionalidad de login

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import styles from './loginform.module.css';
+import { useAuth } from '../../auth/AuthProvider';
 
 export const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
 
-    const handleSubmit = (e) => {
+    const { login } = useAuth()
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        try {
+            setError(null)
+            await login(username, password)
+        } catch (error) {
+            setError('Credenciales no validas, intenta de nuevo')
+        }
     }
 
     return (
         <div className={styles.formContainer}>
-            <form className={styles.form}>
-                <h1>Login</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <h1>Iniciar Sesion</h1>
+
+                {error && <p className={styles.errorMessage}>{error}</p>}
 
                 <label className={styles.label} >Username</label>
                 <input
@@ -30,7 +42,7 @@ export const LoginForm = () => {
                     className={styles.inputPassword}
                 />
 
-                <button className={styles.button}>Iniciar sesion</button>
+                <button type='submit' className={styles.button}>Iniciar sesion</button>
             </form>
 
         </div>
