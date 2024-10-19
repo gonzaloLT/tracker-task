ae85975838a43bcde1c3cdea9bc1fc39bd3ce2a9
Gonzalo Barroso <gonzalobarroso50@gmail.com>
2024-10-19 03:07:37 -0300

    Arreglando error de cerrar sesion al actualizar

--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,37 +1,58 @@
 import { useContext, createContext, useState, useEffect } from "react"
+import { jwtDecode } from "jwt-decode"
 import { API_URL } from "./constants"
 
 const AuthContext = createContext({
     isAuthenticated: false,
-    login: async () =>{},
-    logout: () =>{},
+    login: async () => { },
+    logout: () => { },
     user: null
 })
 
 
 
-export const AuthProvider = ({ children })=>{
+export const AuthProvider = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [user, setUser] = useState(null)
 
-    const login = async (username, password)=>{
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            try {
+                const decodedToken = jwtDecode(token);
+                const currentTime = Date.now() / 1000;
+
+                if (decodedToken.exp > currentTime) {
+                    setIsAuthenticated(true)
+                    setUser(decodedToken.user)
+                } else {
+                    logout()
+                }
+            } catch (error) {
+                console.log('Error en el token: ', error)
+            }
+
+        }
+    }, [])
+
+    const login = async (username, password) => {
         try {
-            const resp = await fetch(`${API_URL}/login`,{
+            const resp = await fetch(`${API_URL}/login`, {
                 method: 'POST',
-                headers:{
+                headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify({username, password})
+                body: JSON.stringify({ username, password })
             })
-    
+
             const data = await resp.json();
-    
-            if(data.success){
-                localStorage.setItem('token', data.token);
+
+            if (data.status === 'success') {
+                localStorage.setItem('token', data.data.token);
                 setIsAuthenticated(true)
-                setUser(data.user)
-            } else{
-                throw new Error(data.message);
+                setUser(data.data.user)
+            } else {
+                throw new Error(data.data.message);
             }
         } catch (error) {
             console.log('Error en la peticion de login: ', error)
@@ -45,7 +66,7 @@ export const AuthProvider = ({ children })=>{
         setIsAuthenticated(false)
         setUser(null)
     }
-    return <AuthContext.Provider value={{isAuthenticated, login, logout, user}}>
+    return <AuthContext.Provider value={{ isAuthenticated, login, logout, user }}>
         {children}
     </AuthContext.Provider>
 }
